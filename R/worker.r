64a80804376d66652a0e43e779ee72c03cadad62
mschubert <mschu.dev@gmail.com>
2016-06-18 22:18:47 +0100

    reference rzmq namespace in worker

--- R/worker.r
+++ R/worker.r
@@ -6,20 +6,18 @@
 #' @param master     The master address (tcp://ip:port)
 #' @param memlimit   Maximum memory before throwing an error
 worker = function(worker_id, master, memlimit) {
-#    worker_id = commandArgs(TRUE)[1]
-#    master = commandArgs(TRUE)[2]
-#    memlimit = as.integer(commandArgs(TRUE)[3])
-    ulimit::memory_limit(memlimit)
+    # https://github.com/krlmlr/ulimit, not on CRAN
+    if (requireNamespace("ulimit", quietly=TRUE))
+        ulimit::memory_limit(memlimit)
+
     print(master)
     print(memlimit)
-    has_pryr = requireNamespace("pryr", quietly=TRUE)
 
-    library(rzmq)
-    context = init.context()
-    socket = init.socket(context, "ZMQ_REQ")
-    connect.socket(socket, master)
-    send.socket(socket, data=list(id=0, worker_id=worker_id))
-    msg = receive.socket(socket)
+    context = rzmq::init.context()
+    socket = rzmq::init.socket(context, "ZMQ_REQ")
+    rzmq::connect.socket(socket, master)
+    rzmq::send.socket(socket, data=list(id=0, worker_id=worker_id))
+    msg = rzmq::receive.socket(socket)
     fun = msg$fun
     const = msg$const
     seed = msg$seed
@@ -31,7 +29,7 @@ worker = function(worker_id, master, memlimit) {
     counter = 0
 
     while(TRUE) {
-        msg = receive.socket(socket)
+        msg = rzmq::receive.socket(socket)
         if (identical(msg$id, 0))
             break
 
@@ -42,15 +40,15 @@ worker = function(worker_id, master, memlimit) {
         result = lapply(seq_along(msg$id), one_id)
         counter = counter + length(msg$id)
 
-        send.socket(socket, data=list(id = msg$id, result=result))
+        rzmq::send.socket(socket, data=list(id = msg$id, result=result))
 
-        if (has_pryr)
-            print(pryr::mem_used())
+        print(pryr::mem_used())
     }
 
     run_time = proc.time() - start_time
 
-    send.socket(socket, data=list(id=-1, worker_id=worker_id, time=run_time, calls=counter))
+    data = list(id=-1, worker_id=worker_id, time=run_time, calls=counter)
+    rzmq::send.socket(socket, data)
 
     print(run_time)
 }
