bd403b8de3ee7d4ec2501c9db9683a40d1d8707e
mschubert <mschu.dev@gmail.com>
2017-01-06 18:19:27 +0100

    allow common_seed=NULL

--- R/work_chunk.r
+++ R/work_chunk.r
@@ -7,9 +7,11 @@
 #' @param const_args   Constant arguments passed to each call
 #' @param common_seed  A seed offset common to all function calls
 #' @return             A list of call results (or try-error if they failed)
-work_chunk = function(df, fun, const_args, common_seed) {
+work_chunk = function(df, fun, const_args=list(), common_seed=NULL) {
     process_id = function(id) {
-        set.seed(common_seed + as.integer(id))
+        if (!is.null(common_seed))
+            set.seed(common_seed + as.integer(id))
+
         iter = stats::setNames(unlist(df[id,], recursive=FALSE), colnames(df))
         result = try(do.call(fun, c(iter, const_args)))
     }
