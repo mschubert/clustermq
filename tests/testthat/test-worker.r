11a57a97b4a843f9ac60ca7b6304f51ba053b2a6
mschubert <mschu.dev@gmail.com>
2017-01-24 21:24:54 +0000

    add delay for zmq to start up/shut down

--- tests/testthat/test-worker.r
+++ tests/testthat/test-worker.r
@@ -3,6 +3,7 @@ context("worker")
 context = rzmq::init.context()
 socket = rzmq::init.socket(context, "ZMQ_REP")
 rzmq::bind.socket(socket, "tcp://*:55443")
+Sys.sleep(0.5)
 
 start_worker = function(id="1", url="tcp://localhost:55443") {
     p = parallel::mcparallel(worker(id, url, 1024))
@@ -20,12 +21,14 @@ send_common = function() {
 shutdown_worker = function(p, id="1") {
     rzmq::send.socket(socket, list(id="WORKER_STOP"))
     msg = rzmq::receive.socket(socket)
+#    rzmq::disconnect.socket(socket)
     rzmq::send.socket(socket, list()) # already shut down
     testthat::expect_equal(msg$id, "WORKER_DONE")
     testthat::expect_equal(msg$worker_id, id)
     testthat::expect_is(msg$time, "proc_time")
     testthat::expect_is(msg$calls, "numeric")
     parallel::mccollect(p)
+    Sys.sleep(0.5)
 }
 
 test_that("control flow", {
@@ -42,5 +45,16 @@ test_that("common data redirect", {
     testthat::expect_equal(msg$id, "WORKER_UP")
 
     send_common()
+    shutdown_worker(p)
+})
+
+test_that("do work", {
+    p = start_worker()
+    send_common()
+
+    rzmq::send.socket(socket, list(id="DO_CHUNK", x=5))
+    msg = rzmq::receive.socket(socket)
+    print(msg)
+
     shutdown_worker(p)
 })
