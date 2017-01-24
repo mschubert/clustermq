afe7e771ca771ca3650be85304886f14ff4e585d
mschubert <mschu.dev@gmail.com>
2017-01-24 20:58:57 +0000

    add common data redirect

--- tests/testthat/test-worker.r
+++ tests/testthat/test-worker.r
@@ -4,25 +4,43 @@ context = rzmq::init.context()
 socket = rzmq::init.socket(context, "ZMQ_REP")
 rzmq::bind.socket(socket, "tcp://*:55443")
 
-test_that("control flow", {
-	worker_id = "1"
-	p = parallel::mcparallel(worker(worker_id, "tcp://localhost:55443", 1024))
+start_worker = function(id="1", url="tcp://localhost:55443") {
+	p = parallel::mcparallel(worker(id, url, 1024))
+	msg = rzmq::receive.socket(socket)
+	testthat::expect_equal(msg$id, "WORKER_UP")
+    p
+}
 
+send_common = function() {
+	rzmq::send.socket(socket, list(fun=function(x) x, const=list(), seed=1))
+	msg = rzmq::receive.socket(socket)
+	testthat::expect_equal(msg$id, "WORKER_READY")
+}
+
+shutdown_worker = function(p, id="1") {
+	rzmq::send.socket(socket, list(id="WORKER_STOP"))
+	msg = rzmq::receive.socket(socket)
+    rzmq::send.socket(socket, "invalid") # already shut down
+	testthat::expect_equal(msg$id, "WORKER_DONE")
+	testthat::expect_equal(msg$worker_id, id)
+	testthat::expect_is(msg$time, "proc_time")
+	testthat::expect_is(msg$calls, "numeric")
+	parallel::mccollect(p)
+}
+
+test_that("control flow", {
+    p = start_worker()
+    send_common()
+    shutdown_worker(p)
+})
+
+test_that("common data redirect", {
+    p = start_worker()
+
+	rzmq::send.socket(socket, list(redirect="tcp://localhost:55443"))
 	msg = rzmq::receive.socket(socket)
 	testthat::expect_equal(msg$id, "WORKER_UP")
 
-	rzmq::send.socket(socket, list(fun=function(x) x, const=list(), seed=1))
-
-	msg = rzmq::receive.socket(socket)
-	testthat::expect_equal(msg$id, "WORKER_READY")
-
-	rzmq::send.socket(socket, list(id="WORKER_STOP"))
-
-	msg = rzmq::receive.socket(socket)
-	testthat::expect_equal(msg$id, "WORKER_DONE")
-	testthat::expect_equal(msg$worker_id, worker_id)
-	testthat::expect_is(msg$time, "proc_time")
-	testthat::expect_is(msg$calls, "numeric")
-
-	parallel::mccollect(p)
+    send_common()
+    shutdown_worker(p)
 })
