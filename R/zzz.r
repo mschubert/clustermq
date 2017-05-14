75d649d376aec845aebc49904c875545d42bf8f9
mschubert <mschu.dev@gmail.com>
2017-05-14 23:37:48 +0100

    supply default option

--- R/zzz.r
+++ R/zzz.r
@@ -5,12 +5,10 @@
 #' its setup() function if it exists, and set the variable 'qsys_id' to
 #' the scheduler we use
 .onLoad = function(libname, pkgname) {
-    qsys_id = toupper(getOption('clustermq.scheduler'))
+    qsys_id = toupper(getOption('clustermq.scheduler', default="local"))
     assign("qsys_id", qsys_id, envir=parent.env(environment()))
 
-    if (length(qsys_id) == 0) {
-        qsys_id = "LOCAL"
-
+    if (qsys_id == "LOCAL") {
         packageStartupMessage("* Option 'clustermq.scheduler' not set, ",
                 "defaulting to ", sQuote(qsys_id))
         packageStartupMessage("--- see: https://github.com/mschubert/clustermq/wiki#setting-up-the-scheduler")
