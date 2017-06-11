d33951e9d73fbbc0fa4922cea9837d8f391867e7
mschubert <mschu.dev@gmail.com>
2017-06-11 15:58:00 +0100

    use qsys avail; warn if no opt set on attach (fixes #35)

--- R/zzz.r
+++ R/zzz.r
@@ -8,11 +8,10 @@
     qsys_id = toupper(getOption('clustermq.scheduler'))
 
     if (length(qsys_id) == 0) {
-        qsys_id = "LOCAL"
-
-        packageStartupMessage("* Option 'clustermq.scheduler' not set, ",
-                "defaulting to ", sQuote(qsys_id))
-        packageStartupMessage("--- see: https://github.com/mschubert/clustermq/wiki#setting-up-the-scheduler")
+        qname = c("SLURM", "LSF", "SGE", "LOCAL")
+        exec = c(Sys.which(c("sbatch", "bsub", "qsub")), "/bin/true")
+        select = which(nchar(exec) > 0)[1]
+        qsys_id = qname[select]
     }
 
     qsys = tryCatch(parent.env(environment())[[qsys_id]],
@@ -23,4 +22,13 @@
 
     assign("qsys_id", qsys_id, envir=parent.env(environment()))
     assign("qsys", qsys, envir=parent.env(environment()))
+}
+
+#' Report queueing system on package attach if not set
+.onAttach = function(libname, pkgname) {
+    if (!is.null(getOption("clustermq.scheduler"))) {
+        packageStartupMessage("* Option 'clustermq.scheduler' not set, ",
+                "defaulting to ", sQuote(qsys_id))
+        packageStartupMessage("--- see: https://github.com/mschubert/clustermq/wiki#setting-up-the-scheduler")
+    }
 }
