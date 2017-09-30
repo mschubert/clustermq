54a342b9dc195da19d91ff4dd9531cd185d34013
mschubert <mschu.dev@gmail.com>
2017-09-30 21:05:00 +0000

    add test for worker number

--- tests/testthat/test-qsys.r
+++ tests/testthat/test-qsys.r
@@ -25,3 +25,12 @@ test_that("seed reproducibility", {
     r2 = Q(fx, x=1:3, n_jobs=1, qsys_id="multicore")
     expect_equal(r1, r2)
 })
+
+test_that("master does not exit loop prematurely", {
+    fx = function(x) {
+        Sys.sleep(0.5)
+        x*2
+    }
+    result = Q(fx, x=1:3, n_jobs=2, qsys_id="multicore")
+    expect_equal(result, as.list(1:3*2))
+})
