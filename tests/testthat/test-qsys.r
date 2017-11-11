79850a402320e2fa8436a4e7a11e820165a39a1a
mschubert <mschu.dev@gmail.com>
2017-11-11 16:23:24 +0100

    fix CRAN winbuilder

--- tests/testthat/test-qsys.r
+++ tests/testthat/test-qsys.r
@@ -1,13 +1,14 @@
 context("qsys")
-skip_on_os("windows")
 
 test_that("control flow", {
+    skip_on_os("windows")
     fx = function(x) x*2
     r = Q(fx, x=1:3, workers=workers(n_jobs=1, qsys_id="multicore", reuse=FALSE))
     expect_equal(r, as.list(1:3*2))
 })
 
 test_that("common data", {
+    skip_on_os("windows")
     fx = function(x, y) x*2 + y
     r = Q(fx, x=1:3, const=list(y=10),
           workers = workers(n_jobs=1, qsys_id="multicore", reuse=FALSE))
@@ -15,6 +16,7 @@ test_that("common data", {
 })
 
 test_that("export", {
+    skip_on_os("windows")
     fx = function(x) x*2 + z
     r = Q(fx, x=1:3, export=list(z=20),
           workers = workers(n_jobs=1, qsys_id="multicore", reuse=FALSE))
@@ -22,6 +24,7 @@ test_that("export", {
 })
 
 test_that("seed reproducibility", {
+    skip_on_os("windows")
     fx = function(x) sample(1:100, 1)
     r1 = Q(fx, x=1:3, workers=workers(n_jobs=1, qsys_id="multicore", reuse=FALSE))
     r2 = Q(fx, x=1:3, workers=workers(n_jobs=1, qsys_id="multicore", reuse=FALSE))
@@ -29,6 +32,7 @@ test_that("seed reproducibility", {
 })
 
 test_that("master does not exit loop prematurely", {
+    skip_on_os("windows")
     fx = function(x) {
         Sys.sleep(0.5)
         x*2
