5e00cfc806278fb062b0d5162a980d1e7c69d2f5
mschubert <mschu.dev@gmail.com>
2017-11-11 14:35:45 +0000

    tag 0.8.0

--- R/master.r
+++ R/master.r
@@ -50,8 +50,8 @@ master = function(qsys, iter, fail_on_error=TRUE, wait_time=NA, chunk_size=NA) {
         switch(msg$id,
             "WORKER_UP" = {
                 if (msg$pkgver != pkgver && pkg_warn) {
-                    warning("Version mismatch: master has ", pkgver,
-                            " worker ", msg$pkgver, immediate.=TRUE)
+                    warning("\nVersion mismatch: master has ", pkgver,
+                            ", worker ", msg$pkgver, immediate.=TRUE)
                     pkg_warn = FALSE
                 }
                 qsys$send_common_data()
