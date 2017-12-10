8fde37b31da0653cb4ce55e8270920451f4c64c8
mschubert <mschu.dev@gmail.com>
2017-12-10 20:19:30 +0000

    guard against unclean exit

--- tests/testthat/test-ssh_proxy.r
+++ tests/testthat/test-ssh_proxy.r
@@ -11,6 +11,7 @@ test_that("control flow between proxy and master", {
     common_data = list(id="DO_SETUP", fun = function(x) x*2,
             const=list(), export=list(), seed=1)
     p = parallel::mcparallel(ssh_proxy(port, port, 'multicore'))
+    on.exit(tools::pskill(p$pid, tools::SIGKILL))
 
     # startup
     msg = recv(p, socket)
@@ -25,7 +26,7 @@ test_that("control flow between proxy and master", {
     token = msg$token
 
     # command execution
-    cmd = methods::Quote(Sys.getpid())
+    cmd = quote(Sys.getpid())
     send(socket, list(id="PROXY_CMD", exec=cmd))
     msg = recv(p, socket)
     expect_equal(msg$id, "PROXY_CMD")
@@ -48,4 +49,5 @@ test_that("control flow between proxy and master", {
 
     collect = clean_collect(p)
     expect_equal(as.integer(names(collect)), p$pid)
+    on.exit(NULL)
 })
