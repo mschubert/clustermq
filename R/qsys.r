233fcd787221afeb5709403f8110a066617db2b4
mschubert <mschu.dev@gmail.com>
2017-12-10 21:30:05 +0000

    fix mem reporting

--- R/qsys.r
+++ R/qsys.r
@@ -124,18 +124,18 @@ QSys = R6::R6Class("QSys",
         # Compute summary statistics for workers
         summary_stats = function() {
             times = lapply(private$worker_stats, function(w) w$time)
-            mem = sapply(private$worker_stats, function(w) w$mem)
+            max_mem = max(sapply(private$worker_stats, function(w) w$mem))
             wt = Reduce(`+`, times) / length(times)
             rt = proc.time() - private$timer
 
             if (class(wt) != "proc_time")
                 wt = rep(NA, 3)
-            if (!is.numeric(mem) || length(mem) == 1)
-                mem = NA
+            if (length(max_mem) != 1)
+                max_mem = NA
 
             fmt = "Master: [%.1fs %.1f%% CPU]; Worker: [avg %.1f%% CPU, max %.1f Mb]"
             message(sprintf(fmt, rt[[3]], 100*(rt[[1]]+rt[[2]])/rt[[3]],
-                            100*(wt[[1]]+wt[[2]])/wt[[3]], max(mem)))
+                            100*(wt[[1]]+wt[[2]])/wt[[3]], max_mem))
         }
     ),
 
