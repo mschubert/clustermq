649a0cca058704706bc29f3180df295a0703e6a3
mschubert <mschu.dev@gmail.com>
2018-08-31 07:44:07 +0000

    handle NULL on non-critical worker interrupt (fixes #103)

--- R/worker.r
+++ R/worker.r
@@ -33,7 +33,9 @@ worker = function(master, timeout=600, ..., verbose=TRUE) {
 
     while(TRUE) {
         events = rzmq::poll.socket(list(socket), list("read"), timeout=timeout)
-        if (events[[1]]$read) {
+        if (is.null(events[[1]])) {
+            next
+        } else if (events[[1]]$read) {
             tic = proc.time()
             msg = rzmq::receive.socket(socket)
             delta = proc.time() - tic
