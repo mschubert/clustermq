523792760790ff2582c9d5fbf10ae5218df1e87e
mschubert <mschu.dev@gmail.com>
2019-04-03 12:29:24 +0200

    revert ssh test

--- tests/testthat/test-5-qsys_impl.r
+++ tests/testthat/test-5-qsys_impl.r
@@ -30,33 +30,33 @@ test_that("qsys_multicore", {
 })
 
 test_that("qsys_lsf", {
+    skip_on_cran()
     skip_if_not(with(avail, bsub))
     skip_if_not(has_cmq)
     skip_if_not(has_network)
     skip_on_os("windows")
-    skip_on_cran()
     w = workers(n_jobs=1, qsys_id="lsf", reuse=FALSE)
     r = Q(fx, x=1:3, workers=w, timeout=3L)
     expect_equal(r, as.list(1:3*2))
 })
 
 test_that("qsys_sge", {
+    skip_on_cran()
     skip_if_not(with(avail, qsub))
     skip_if_not(has_cmq)
     skip_if_not(has_network)
     skip_on_os("windows")
-    skip_on_cran()
     w = workers(n_jobs=1, qsys_id="sge", reuse=FALSE)
     r = Q(fx, x=1:3, workers=w, timeout=3L)
     expect_equal(r, as.list(1:3*2))
 })
 
 test_that("qsys_slurm", {
+    skip_on_cran()
     skip_if_not(with(avail, sbatch))
     skip_if_not(has_cmq)
     skip_if_not(has_network)
     skip_on_os("windows")
-    skip_on_cran()
     w = workers(n_jobs=1, qsys_id="slurm", reuse=FALSE)
     r = Q(fx, x=1:3, workers=w, timeout=3L)
     expect_equal(r, as.list(1:3*2))
--- tests/testthat/test-6-ssh_proxy.r
+++ tests/testthat/test-6-ssh_proxy.r
@@ -63,10 +63,9 @@ test_that("full SSH connection", {
     sched = getOption("clustermq.scheduler", qsys_default)
     skip_if(is.null(sched) || toupper(sched) == "LOCAL",
             message="options(clustermq.scheduler') can not be 'LOCAL'")
-return()
+
     w = workers(n_jobs=1, qsys_id="ssh", reuse=FALSE,
-                ssh_host="localhost", node="localhost",
-                template=list(ssh_log="~/ssh_proxy.log"))
+                ssh_host="localhost", node="localhost")
     result = Q(identity, 42, n_jobs=1, timeout=10L, workers=w)
     expect_equal(result, list(42))
 })
