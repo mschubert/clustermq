b881b5e114ed7864764aea0eed9b1c91951c68fc
mschubert <mschu.dev@gmail.com>
2019-07-16 15:30:16 +0200

    add is_trivially_copyable workaround

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -3,6 +3,13 @@
 #include <string>
 #include "zmq.hpp"
 
+// workaround missing "is_trivially_copyable" in g++ < 5.0
+#if __GNUG__ && __GNUC__ < 5
+#define IS_TRIVIALLY_COPYABLE(T) __has_trivial_copy(T)
+#else
+#define IS_TRIVIALLY_COPYABLE(T) std::is_trivially_copyable<T>::value
+#endif
+
 typedef std::chrono::high_resolution_clock Time;
 typedef std::chrono::milliseconds ms;
 
