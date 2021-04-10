4e61cb031eb308cddcc09da59b15656223e1e257
mschubert <mschu.dev@gmail.com>
2021-04-10 23:05:45 +0200

    add and poll disc monitor

--- src/CMQWorker.cpp
+++ src/CMQWorker.cpp
@@ -7,8 +7,16 @@ public:
         sock = zmq::socket_t(*ctx, ZMQ_REQ);
         sock.set(zmq::sockopt::connect_timeout, 10000);
         sock.connect(addr);
+
+        int rc = zmq_socket_monitor(sock, "inproc://monitor", ZMQ_EVENT_DISCONNECTED);
+        if (rc < 0) // C API needs return value check
+            Rf_error("failed to create socket monitor");
+        mon = zmq::socket_t(*ctx, ZMQ_PAIR);
+        mon.connect("inproc://monitor");
     }
     ~CMQWorker() {
+        mon.set(zmq::sockopt::linger, 0);
+        mon.close();
         sock.set(zmq::sockopt::linger, 0);
         sock.close();
         ctx->close();
@@ -23,6 +31,7 @@ public:
         sock.send(content, zmq::send_flags::none);
     }
     SEXP receive() {
+        poll();
         zmq::message_t msg;
         sock.recv(msg, zmq::recv_flags::none);
         SEXP ans = Rf_allocVector(RAWSXP, msg.size());
@@ -31,6 +40,7 @@ public:
     }
 
     SEXP get_data_redirect(std::string addr, SEXP data) {
+        //todo: this should ideally also be monitored (although short connection)
         auto rsock = zmq::socket_t(*ctx, ZMQ_REQ);
         rsock.set(zmq::sockopt::connect_timeout, 10000);
         rsock.connect(addr);
@@ -57,6 +67,28 @@ public:
 private:
     zmq::context_t *ctx;
     zmq::socket_t sock;
+    zmq::socket_t mon;
+
+    void poll() {
+        auto pitems = std::vector<zmq::pollitem_t>(2);
+        pitems[0].socket = sock;
+        pitems[0].events = ZMQ_POLLIN;
+        pitems[1].socket = mon;
+        pitems[1].events = ZMQ_POLLIN;
+
+        int total_sock_ev = 0;
+        do {
+            try {
+                zmq::poll(pitems, std::chrono::duration<long int>::max());
+            } catch (zmq::error_t const &e) {
+                if (errno != EINTR || pending_interrupt())
+                    Rf_error(e.what());
+            }
+            if (pitems[1].revents > 0)
+                Rf_error("Unexpected peer disconnect");
+            total_sock_ev = pitems[0].revents;
+        } while (total_sock_ev == 0);
+    }
 };
 
 RCPP_MODULE(cmq_worker) {
