db6cb8c7984b77a0e985194467c25eed6c378239
mschubert <mschu.dev@gmail.com>
2022-11-18 17:14:18 +0100

    add test for multiprocess

--- tests/testthat/test-3-pool.r
+++ tests/testthat/test-3-pool.r
@@ -8,3 +8,12 @@ test_that("starting and stopping multicore", {
     w$send_shutdown()
     w$cleanup()
 })
+
+test_that("multiprocess", {
+    w = workers(1, qsys_id="multiprocess")
+    expect_null(w$recv())
+    w$send(expression(3 + 5))
+    expect_equal(w$recv(), 8)
+    w$send_shutdown()
+    w$cleanup()
+})
