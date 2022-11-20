2920350468ebf1cc9d70d3c7e15d2ada539b9977
mschubert <mschu.dev@gmail.com>
2022-11-20 13:48:45 +0100

    no multicore on windows

--- tests/testthat/test-4-pool.r
+++ tests/testthat/test-4-pool.r
@@ -1,6 +1,8 @@
 context("pool")
 
 test_that("starting and stopping multicore", {
+    skip_on_os("windows")
+
     w = workers(1, qsys_id="multicore")
     expect_null(w$recv())
     w$send(expression(3 + 4))
