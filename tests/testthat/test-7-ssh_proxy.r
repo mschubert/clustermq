9f218609d4366e2f6a9783a8e0f5de51837d7bdd
mschubert <mschu.dev@gmail.com>
2023-03-24 11:49:54 +0100

    disable 3+4

--- tests/testthat/test-7-ssh_proxy.r
+++ tests/testthat/test-7-ssh_proxy.r
@@ -45,6 +45,7 @@ test_that("proxy communication yields submit args", {
 })
 
 test_that("using the proxy without pool and forward", {
+    skip("ci isolate")
     skip_on_cran()
     skip_on_os("windows")
 #    skip_if_not(has_localhost)
@@ -73,6 +74,7 @@ test_that("using the proxy without pool and forward", {
 })
 
 test_that("full SSH connection", {
+    skip("ci isolate")
     skip_on_cran()
     skip_on_os("windows")
 #    skip_if_not(has_localhost)
