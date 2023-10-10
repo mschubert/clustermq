9257e98611ff069e0a9b1e75d51d0428127c9432
mschubert <mschu.dev@gmail.com>
2023-10-10 14:45:36 +0200

    allow compilation without zeromq DRAFT option

--- src/CMQMaster.h
+++ src/CMQMaster.h
@@ -14,7 +14,9 @@ public:
     std::string listen(Rcpp::CharacterVector addrs) {
         sock = zmq::socket_t(*ctx, ZMQ_ROUTER);
         sock.set(zmq::sockopt::router_mandatory, 1);
+        #if defined ZMQ_ROUTER_NOTIFY
         sock.set(zmq::sockopt::router_notify, ZMQ_NOTIFY_DISCONNECT);
+        #endif
 
         int i;
         for (i=0; i<addrs.length(); i++) {
