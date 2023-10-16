a84388ea5279ef2cd26a1d06efa55ad51deba403
mschubert <mschu.dev@gmail.com>
2023-10-16 18:58:35 +0200

    ignore template args on 'local' scheduler

--- R/qsys_local.r
+++ R/qsys_local.r
@@ -7,9 +7,9 @@ LOCAL = R6::R6Class("LOCAL",
     inherit = QSys,
 
     public = list(
-        initialize = function(addr="unused", n_jobs=0, ..., log_worker=FALSE,
-                              log_file=NULL, verbose=TRUE) {
-            super$initialize(addr=addr, ...)
+        initialize = function(addr="unused", n_jobs=0, master=NULL, ...,
+                              log_worker=FALSE, log_file=NULL, verbose=TRUE) {
+            super$initialize(addr=addr, master=master)
             if (verbose)
                 message("Running sequentially ('LOCAL') ...")
         },
