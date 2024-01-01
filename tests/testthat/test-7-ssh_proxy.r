a39b67c33010e8e717714a6a2e37b8a3cb428d57
Michael Schubert <mschu.dev@gmail.com>
2024-01-01 23:17:39 +0100

    increase timeouts for mac CI

--- tests/testthat/test-7-ssh_proxy.r
+++ tests/testthat/test-7-ssh_proxy.r
@@ -62,9 +62,9 @@ test_that("using the proxy without pool and forward", {
     addr = m$listen("tcp://127.0.0.1:*")
     p = parallel::mcparallel(ssh_proxy(sub(".*:", "", addr)))
 
-    m$proxy_submit_cmd(list(n_jobs=1), 5000L)
+    m$proxy_submit_cmd(list(n_jobs=1), 10000L)
     m$add_pending_workers(1L)
-    expect_null(m$recv(1000L)) # worker 1 up
+    expect_null(m$recv(2000L)) # worker 1 up
     m$send(5 + 2)
     expect_equal(m$recv(500L), 7) # collect results
 
@@ -86,9 +86,9 @@ test_that("using the proxy without pool and forward, 2 workers", {
     addr = m$listen("tcp://127.0.0.1:*")
     p = parallel::mcparallel(ssh_proxy(sub(".*:", "", addr)))
 
-    m$proxy_submit_cmd(list(n_jobs=2), 5000L)
+    m$proxy_submit_cmd(list(n_jobs=2), 10000L)
     m$add_pending_workers(2L)
-    expect_null(m$recv(1000L)) # worker 1 up
+    expect_null(m$recv(2000L)) # worker 1 up
     m$send({ Sys.sleep(0.5); 5 + 2 })
     expect_null(m$recv(500L)) # worker 2 up
     m$send({ Sys.sleep(0.5); 3 + 1 })
